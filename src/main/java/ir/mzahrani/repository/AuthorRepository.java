c609312c100d3a261334e82e94dea5ada14c1eb0
mahdi <mahdizahrani76@gmail.com>
2025-10-04 10:04:17 +0330

    feat: add annotation and author repository ( refs #6)

---
+++ src/main/java/ir/mzahrani/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package ir.mzahrani.repository;
+
+import ir.mzahrani.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
